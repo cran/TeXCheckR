7779c46c3c7642d27b6d20c0f269c476c0034278
Hugh Parsonage <hugh.parsonage@gmail.com>
2017-11-04 22:38:00 +0000

    version 0.4.4

--- NEWS.md
+++ NEWS.md
@@ -1,3 +1,12 @@
+# TeXCheckR 0.4.3
+* Use `hunspell`'s ignore option when a dictionary addition is not respected
+* Fix spurious unit test broken under new `hunspell`.
+* Fix issue with `check_footnote_typography` where the RStudio pops to the end of the first footnote, not the first wrong footntoe. Thanks to CC for reporting.
+
+# TeXCheckR 0.4.2
+* Fixed `inputs_of` to select `\include` and `\inputs` only
+* Export `inputs_of`
+
 # TeXCheckR 0.4.1
 * Fixed an issue whereby `pdflatex` may not run in certain environments (in particular CRAN where it caused an egregious hanging of the package queue). Tests now skip in such cases. 
 
--- R/inputs_of.R
+++ R/inputs_of.R
@@ -1,8 +1,19 @@
+#' Inputs to files nested within LaTeX document
+#' @param filename The file whose \code{\\inputs} are to be extracted.
+#' @param exclude.preamble (logical) If \code{TRUE}, the default, only \code{\\input}s and \code{\\include}s within the \code{document} environment are returned.
+#' @param append.tex Should the result include the file extension \code{.tex}? By default, \code{TRUE}. Setting to \code{FALSE} may be useful when the file is not a \code{.tex} file.
+#' @return A character vector of file paths relative to \code{filename} that are
+#'  used as \code{\\input}s or \code{\\include}s within \code{filename}. If 
+#'  no such files are present within \code{filename}, \code{NULL} is returned.
+#' @export
 
-
-inputs_of <- function(filename, exclude.preamble = TRUE, append.tex = TRUE){
-  file_path <- dirname(filename)
+inputs_of <- function(filename, exclude.preamble = TRUE, append.tex = TRUE) {
   lines <- read_lines(filename)
+  
+  if (any(grepl("\\end{document}", lines, fixed = TRUE))) {
+    lines <- 
+      lines[seq_along(lines) < grep("\\end{document}", lines, fixed = TRUE)]
+  }
   
   lines_after_begin_document <-
     if (exclude.preamble && any(grepl("\\begin{document}", lines, fixed = TRUE))){
@@ -17,23 +28,32 @@ inputs_of <- function(filename, exclude.preamble = TRUE, append.tex = TRUE){
                                perl = TRUE))
   
   if (inputs_in_doc > 0) {
-    inputs <- gsub("^\\\\(?:(?:input)|(?:include(?!(?:graphics))))[{](.*(?:\\.tex)?)[}]$",
-                   "\\1",
-                   lines_after_begin_document[grepl("^\\\\(?:(?:input)|(?:include(?!(?:graphics))))[{](.*(\\.tex)?)[}]$",
-                                                    lines_after_begin_document,
-                                                    perl = TRUE)],
-                   perl = TRUE)
+    lines_with_possible_inputs <-
+      grep("\\\\(?:(?:input)|(?:include))",
+           lines_after_begin_document,
+           perl = TRUE,
+           value = TRUE)
     
-    if (length(inputs) != inputs_in_doc){
-      stop("Unable to parse inputs. Check they are all of the form \\input{filename}.", "\n", 
-           "\\input inside a comment will NOT be ignored.")
-    }
+    out <-
+      lapply(c("input", "include"), 
+             extract_LaTeX_argument, 
+             tex_lines = lines_with_possible_inputs) %>%
+      rbindlist
     
-    if (append.tex){
-      inputs <- paste0(inputs, ".tex")
-    }
+      if (nrow(out) > 0) {
+        out <- 
+          out %>%
+          setorderv("line_no_min") %>%
+          .[["extract"]]
+        
+        if (append.tex) {
+          out <- sprintf("%s.tex", tools::file_path_sans_ext(out))
+        }
+      } else {
+        out <- NULL
+      }
     
-    return(inputs)
+    return(out)
   } else {
     return(NULL)
   }
